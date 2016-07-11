b7ffd2a0bc2bbcdd15d8b0e3c195b0e6036ed721
amshenoy <abmshenoy@gmail.com>
2016-07-11 07:41:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
 })
 
 // for facebook verification
-app.get('/webhook/', function (req, res) {
+app.get('/hook/', function (req, res) {
 	if (req.query['hub.verify_token'] === 'lucid_is_awesome') {
 		res.send(req.query['hub.challenge']);
 	}
@@ -27,7 +27,7 @@ app.get('/webhook/', function (req, res) {
 })
 
 // to post data
-app.post('/webhook/', function (req, res) {
+app.post('/hook/', function (req, res) {
 	messaging_events = req.body.entry[0].messaging
 	for (i = 0; i < messaging_events.length; i++) {
 		event = req.body.entry[0].messaging[i];
